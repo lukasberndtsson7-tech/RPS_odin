1fc981b2252bea5a51326739593216cef91c5bd9
Lukas <lukasberndtsson7@gmail.com>
2026-02-07 22:39:49 +0100

    Refactor getComputerChoice function and implement game logic for player vs computer rounds

--- javascript/rps.js
+++ javascript/rps.js
@@ -1,15 +1,64 @@
 function getComputerChoice() {
  let compChoice = Math.random();
- if (compChoice < 0.33){
-    console.log("rock");
+ if (compChoice < .33){
+    compChoice = "rock";
+    return compChoice;
  } 
-else if( 0.33 < compChoice < 0.66){
-    console.log("paper");
+else if(compChoice >= .33 && compChoice < .66){
+    compChoice = "paper";
+    return compChoice;
 }
-else if(0.66 < compChoice){ 
-    console.log("scissors");
+else if(compChoice >= .66){
+    compChoice = "scissors";
+    return compChoice;
 }
 
 }
 
-getComputerChoice()
+function getPlayerChoice(){
+  let playerChoice = prompt("rock, paper or scissors")
+  playerChoice = playerChoice.toLowerCase();
+   return playerChoice;
+    }
+let humanScore = 0;
+let compscore = 0;
+
+function playRound(round, humanChoice, computerChoice) {
+if(humanChoice == computerChoice){
+    console.log("ROUND:" + round + "\nIt is a tie\n" + `computer chose: ${computerChoice}\nyou chose: ${humanChoice} ` +  " you: " + humanScore + " computer: " + compscore);
+}
+else if(computerChoice == "rock" && humanChoice == "scissors" ||
+        computerChoice == "paper" && humanChoice == "rock" ||
+        computerChoice == "scissors" && humanChoice == "paper"){
+            compscore += 1;
+console.log("ROUND:" + round + "\nYou lose\n" + `computer chose: ${computerChoice}\nyou chose: ${humanChoice} ` + "you: " + humanScore + " computer: " + compscore)
+}
+else {
+    humanScore += 1;
+console.log("ROUND:" + round + "\nYou win\n" + `computer chose: ${computerChoice}\nyou chose: ${humanChoice} ` + "you: " + humanScore + " computer: " + compscore)
+}
+
+
+}
+
+
+
+
+for (var i = 1; i <= 5; i++){
+    let humanSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+    playRound(i, humanSelection, computerSelection)}
+
+    function winner(){
+    if(compscore == humanScore){
+        console.log("IT IS A TIE!!!")
+    }
+    if(compscore < humanScore){
+        console.log("YOOOOUUUU LOOOOSSSSSSSSSSEEEEEEEEEE!!!")
+    }
+    if(compscore > humanScore){
+         console.log("you win")
+    }
+
+    }
+    winner()
